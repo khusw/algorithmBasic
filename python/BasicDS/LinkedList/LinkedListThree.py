2ba1487a55b850f7e6e03cd751155bd823435bfe
sdy <vel1024@khu.ac.kr>
2020-06-29 18:21:08 +0900

    add delete function

--- python/BasicDS/LinkedList/LinkedListThree.py
+++ python/BasicDS/LinkedList/LinkedListThree.py
@@ -33,6 +33,26 @@ class NodeMgmt:
             node = node.next
         print(node.data)
 
+    def delete(self, data):  # 노드를 삭제하기 위한 함수
+        if self.head == "":
+            print("there is no node")
+            return
+
+        if self.head.data == data:  # head 를 지우려 할 때
+            temp = self.head
+            self.head = self.head.next
+            del temp  # python 은 객체 인스턴스 지울때 del 사용
+        else:  # head 가 아닌 다른 데이터를 지울 때 (중간, 끝)
+            node = self.head
+            while node.next:
+                if node.next.data == data:
+                    temp = node.next
+                    node.next = node.next.next
+                    del temp
+                    return  # 찾아서 삭제 했으므로 리턴
+                else:
+                    node = node.next  # 다음 loop 를 돌기 위한 구문
+
 
 linkedlist1 = NodeMgmt(0)
 linkedlist1.add(1)
@@ -43,3 +63,23 @@ for index in range(4, 11):
     linkedlist1.add(index)
 
 linkedlist1.desc()
+
+'''
+    노드 삭제
+    : head 를 삭제하는 경우, 중간 노드를 삭제하는 경우, 마지막 노드를 삭제하는 경우    
+'''
+
+linkedlist2 = NodeMgmt(0)
+print(linkedlist2.head)
+
+# head 를 지우는 경우
+linkedlist2.delete(0)  # head 값 0 을 제거
+
+for index in range(1, 5):
+    linkedlist2.add(index)
+
+linkedlist2.desc()  # 전체 1 ~ 5 출력
+
+linkedlist2.delete(4)  # 4를 삭제 (중간 노드 삭제)
+
+linkedlist2.desc()  # 4 를 제외한 1,2,3,5 를 출력
