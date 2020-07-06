79455f651403e38695da45dfe6331da8f1b2b83c
sdy <vel1024@khu.ac.kr>
2020-07-06 12:31:34 +0900

    add vertex connection

--- cpp/BFS/Basic.cpp
+++ cpp/BFS/Basic.cpp
@@ -57,11 +57,20 @@ int main()
     a[2].push_back(5);
     a[5].push_back(2);
 
+    a[4].push_back(5);
+    a[5].push_back(4);
+
     a[3].push_back(6);
     a[6].push_back(3);
 
     a[3].push_back(7);
     a[7].push_back(3);
+
+    a[6].push_back(7);
+    a[7].push_back(6);
+
+    bfs(1);
+    return 0;
 }
 
 /*
