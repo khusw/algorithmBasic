1da634c8a411649bf60843eea6dba2ffdf26d1ef
sdy <vel1024@khu.ac.kr>
2020-09-07 19:44:01 +0900

    create BOJ 7662

---
+++ cpp/BasicDS/DualPriorityQueue/DualPriorityQueue.cpp
@@ -0,0 +1,50 @@
+/* 
+    BOJ 7662 이중 우선순위 큐
+    https://www.acmicpc.net/problem/7662
+*/
+
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int T, k, num;
+char ch;
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    cin >> T;
+    for (int i = 0; i < T; i++) {
+        cin >> k;
+        multiset<int> ms;
+        for (int j = 0; j < k; j++) {
+            cin >> ch >> num;
+            if (ch == 'I') {
+                ms.insert(num);
+            } else {
+                if (!ms.empty()) {
+                    if (num == 1) {
+                        // 최대 값 삭제
+                        auto iter = ms.end();
+                        iter--;
+                        ms.erase(iter);
+                    } else {
+                        // 최소 값 삭제
+                        ms.erase(ms.begin());
+                    }
+                }
+            }
+        }
+        if (ms.empty()) {
+            cout << "EMPTY" << '\n';
+        } else {
+            auto iter = ms.end();
+            iter--;
+            cout << *iter << " " << *ms.begin() << '\n';
+        }
+    }
+
+    return 0;
+}
