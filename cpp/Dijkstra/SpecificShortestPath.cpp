4b13f31842215812fc9358409a05052b7bf5c8f6
sdy <vel1024@khu.ac.kr>
2020-09-17 15:10:04 +0900

    change dijkstra code

--- cpp/Dijkstra/SpecificShortestPath.cpp
+++ cpp/Dijkstra/SpecificShortestPath.cpp
@@ -14,20 +14,11 @@
 
     (A, B 는 반드시 거쳐야할 중간 노드)
 
-    첫번째
-    : 1에서 A 또는 B 를 가기 위한 최단 거리 값
-
-    두번째
-    : A 에서 B 로 가기 위한 최단 거리값
-    (방향이 없는 그래프여서 A->B 나 B->A 나 가중치 값이 같다)
-
-    세번째
-    : A 에서 N 가기 위한 최단 거리 값
-
-    네번째
-    : B 에서 N 가기 위한 최단 거리 값
-
-    이렇게 4개의 시행을 거쳐야 원하는 조건이 성립.
+    여기서는 dijkstra(시작점, 끝점)
+    으로 처리해서
+    1 -> A 의 값
+    A -> B 의 값
+    B -> N 의 값 을 구하는 방식으로 처리했다.
 */
 
 #include <iostream>
@@ -41,12 +32,11 @@
 using namespace std;
 typedef pair<int, int> P;
 
-int N, E, mid1, mid2, answer;
-bool check, check2;
+int N, E, A, B, answer;
 vector<pair<int, int>> adj[MAX];  // node 번호, 거리 순 으로 들어감
-int dist[MAX];
 
-void dijkstra(int start) {
+int dijkstra(int start, int end) {
+    vector<int> dist(N + 1, INF);
     dist[start] = 0;
     priority_queue<P, vector<P>, greater<P>> pq;  // 거리 , 노드 번호 순으로 들어감
     pq.push(make_pair(0, start));
@@ -54,9 +44,6 @@ void dijkstra(int start) {
         int node = pq.top().second;
         int node_distance = pq.top().first;
         pq.pop();
-
-        if (dist[node] < node_distance)
-            continue;
 
         for (int i = 0; i < adj[node].size(); i++) {
             int next_node = adj[node][i].first;
@@ -68,49 +55,47 @@ void dijkstra(int start) {
             }
         }
     }
+    return dist.at(end);
 }
 
 int main() {
     cin.tie(0);
     ios_base::sync_with_stdio(0);
 
+    bool check1 = true;
+    bool check2 = true;
+
     cin >> N >> E;
     for (int i = 0; i < E; i++) {
         int a, b, c;
         cin >> a >> b >> c;
         adj[a].push_back(make_pair(b, c));
+        adj[b].push_back(make_pair(a, c));
     }
 
-    for (int i = 1; i <= N; i++) dist[i] = INF;
+    cin >> A >> B;
 
-    cin >> mid1 >> mid2;
+    int start_a = dijkstra(1, A);  // 1 -> A
+    int a_b = dijkstra(A, B);      // A -> B
+    int b_N = dijkstra(B, N);      // B -> N
 
-    dijkstra(1);              // 시작 지점 부터 일단 순회
-    int route1 = dist[mid1];  // 1 -> A 거리
-    int route2 = dist[mid2];  // 1 -> B 거리
+    if (start_a == INF || a_b == INF || b_N == INF) check1 = false;  // 여기까지가 1 -> A -> B -> N 에 대한 검증
 
-    if (route1 != INF) check = true;  // 1 -> A 최단 거리가 있다면 true 로 변환
-    if (route2 != INF) check2 = true;
+    int start_b = dijkstra(1, B);  // 1 -> B
+    int b_a = dijkstra(B, A);      // B -> A
+    int a_N = dijkstra(A, N);      // A -> N
 
-    for (int i = 1; i <= N; i++) dist[i] = INF;  // 거리 값은 항상 다시 리셋해야함.
-    dijkstra(mid1);                              // A 를 시작점으로 하는 다익스트라 계산
-    if (check) route1 += dist[mid2];             // 1 -> A -> B
-    if (check2) route2 += dist[mid1];            // 1 -> B -> A
+    if (start_b == INF || b_a == INF || a_N == INF) check2 = false;
 
-    for (int i = 1; i <= N; i++) dist[i] = INF;
-    dijkstra(mid2);                // B 를 시작점으로 하는 다익스트라 계산
-    if (check) route1 += dist[N];  // 1 -> A -> B -> N
-
-    for (int i = 1; i <= N; i++) dist[i] = INF;
-    dijkstra(mid1);                 // A 를 시작점으로 하는 다익스트라 계산
-    if (check2) route2 += dist[N];  // 1 -> B -> A -> N
-
-    if (!check && !check2)
-        cout << "-1";
-    else {
-        answer = MIN(route1, route2);
-        cout << answer;
+    if (check1 && check2) {
+        int case1 = start_a + a_b + b_N;
+        int case2 = start_b + b_a + a_N;
+        answer = MIN(case1, case2);
+    } else {
+        answer = -1;
     }
+
+    cout << answer;
 
     return 0;
 }
