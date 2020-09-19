a1737654058cfd3a6f69bd81f040fed8e0b861c4
sdy <vel1024@khu.ac.kr>
2020-09-19 15:05:58 +0900

    remove wrong answer code

--- cpp/Simulation/HalfSnailArray.cpp
+++ cpp/Simulation/HalfSnailArray.cpp
@@ -25,6 +25,9 @@
 
     DFS 인줄 알았으나 단순한 구현 문제
     (이렇게 방향이 정해져서 탐색하는 문제는 DFS/BFS 가 아닐 가능성이 큼) 아래 오른쪽 좌상대각선 이 세가지가 고정되어 있기 때문임.
+
+    이 문제의 이동 방향은 3가지임
+    아래, 오른쪽, 좌상 대각선.
 */
 
 #include <algorithm>
@@ -36,44 +39,47 @@ using namespace std;
 vector<int> solution(int n) {
     vector<int> answer;
 
-    vector<vector<int>> vec(n, vector<int>(n));
-    fill(vec.begin(), vec.end(), vector<int>(n, 0));
+    vector<vector<int>> vec(n + 1, vector<int>(n + 1));
+    fill(vec.begin(), vec.end(), vector<int>(n + 1, 0));
 
-    int value = 1;  // 배열에 넣을 값.
-    int cnt = n;    // n, n - 1, n - 2, .... 1 값으로 점점 하락하면서 value 를 넣어야함.
-    int col = 0;
-    int row = -1;
+    int max_num = (n * (n + 1)) / 2;
+    int top = 1;     // 아직 채워지지않은 가장 윗줄 번호를 의미
+    int bottom = n;  // 아직 채워지지 않은 가장 아랫쪽 번호를 의미
+    int right = 0;   // right 는 층 마다 달라서 현재까지 오른쪽 줄을 채운 횟수로 표현된다
+    int left = 1;    // 아직 채워지지 않은 가장 왼쪽줄 번호를 의미
+    int num = 1;     // 배열내에 채워넣을 변수값이다.
+    int state = 0;   // 어떤 화살표 방향으로 이동할지 표현하기 위한 변수
 
-    while (cnt > 0) {
-        for (int i = row + 1; i < n; i++) {
-            if (vec[i][col] != 0)
-                break;
-            vec[i][col] = value++;
-            row = i;
+    while (num <= max_num) {
+        if (state == 0) {
+            // 아래로 이동
+            for (int i = top; i <= bottom; i++) {
+                vec[i][left] = num++;
+            }
+            top++;
+            left++;
+            state = 1;
+        } else if (state == 1) {
+            // 오른쪽으로 이동
+            for (int i = left; i <= bottom - right; i++) {
+                vec[bottom][i] = num++;
+            }
+            bottom--;
+            state = 2;
+        } else if (state == 2) {
+            // 대각선 좌측 위로 이동
+            for (int i = bottom; i >= top; i--) {
+                vec[i][i - right] = num++;
+            }
+            right++;
+            top++;
+            state = 0;
         }
-        cnt--;
-
-        for (int i = col + 1; i < n; i++) {
-            if (vec[row][i] != 0)
-                break;
-            vec[row][i] = value++;
-            col = i;
-        }
-        cnt--;
-
-        while (vec[row][col] != 0 && row > 1 && col > 1 && (row - col) == 0) {
-            row--;
-            col--;
-            vec[row][col] = value++;
-        }
-        cnt--;
     }
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (vec[i][j] != 0) {
-                answer.push_back(vec[i][j]);
-            }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            answer.push_back(vec[i][j]);
         }
     }
 
