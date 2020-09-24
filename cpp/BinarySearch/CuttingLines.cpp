9693f6f700c2a48f3748f41b4a4d09b1dd0b34c6
sdy <vel1024@khu.ac.kr>
2020-09-24 17:27:33 +0900

    create BOJ 1654

---
+++ cpp/BinarySearch/CuttingLines.cpp
@@ -0,0 +1,50 @@
+/*
+    BOJ 1654 랜선 자르기
+    https://www.acmicpc.net/problem/1654
+
+    BOJ 2805 와 사실상 똑같은 문제
+    https://www.acmicpc.net/problem/2805
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+
+int K, N;
+vector<ll> line_list;
+
+void binary_search(ll start, ll end) {
+    while (start + 1 < end) {
+        ll mid = (start + end) / 2;
+        ll temp = 0;
+        for (int i = 0; i < K; i++) {
+            temp = temp + (line_list[i] / mid);
+        }
+        if (temp >= N) {
+            start = mid;
+        } else {
+            end = mid;
+        }
+    }
+    cout << start << '\n';
+}
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    cin >> K >> N;
+
+    for (int i = 0; i < K; i++) {
+        ll line;
+        cin >> line;
+        line_list.push_back(line);
+    }
+
+    binary_search(0, 2147483648);  // 0 ~ 2^31
+
+    return 0;
+}
