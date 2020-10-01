0f48c68489b562dacc73e81f75d2a502f7a20171
sdy <vel1024@khu.ac.kr>
2020-10-01 14:07:58 +0900

    rename BOJ 1074

--- cpp/Recursion/BOJ1074_khusw.cpp
+++ cpp/Recursion/BOJ1074_khusw.cpp
@@ -1,33 +1,31 @@
-#include <iostream>
+/*
+    BOJ 1074 Z
+    https://www.acmicpc.net/problem/1074
+*/
 #include <cmath>
+#include <iostream>
 
 using namespace std;
 
 int N, r, c, cnt;
 
-void recursion(int row, int col, int range)
-{
-    if (row == r && col == c)
-    {
+void recursion(int row, int col, int range) {
+    if (row == r && col == c) {
         cout << cnt;
         return;
     }
 
-    if (r < row + range && r >= row && c < col + range && c >= col)
-    {
+    if (r < row + range && r >= row && c < col + range && c >= col) {
         recursion(row, col, range / 2);
         recursion(row, col + (range / 2), range / 2);
         recursion(row + (range / 2), col, range / 2);
         recursion(row + (range / 2), col + (range / 2), range / 2);
-    }
-    else
-    {
+    } else {
         cnt += range * range;
     }
 }
 
-int main()
-{
+int main() {
     cin >> N >> r >> c;
     recursion(0, 0, pow(2, N));
     return 0;
