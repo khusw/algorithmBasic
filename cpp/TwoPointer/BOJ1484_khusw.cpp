3d07930a8a200d1c8e115a952a726fba42f02bbd
sdy <vel1024@khu.ac.kr>
2020-10-01 14:28:10 +0900

    rename BOJ 1484

--- cpp/TwoPointer/BOJ1484_khusw.cpp
+++ cpp/TwoPointer/BOJ1484_khusw.cpp
@@ -1,15 +1,16 @@
 /*
+    BOJ 1484 다이어트
+    https://www.acmicpc.net/problem/1484
 */
 #include <bits/stdc++.h>
 
 using namespace std;
 
-const int MAX = 1e5; // 십만
+const int MAX = 1e5;  // 십만
 
 int W;
 
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
@@ -17,29 +18,24 @@ int main()
 
     bool check = false;
 
-    int left = 1;  // 과거
-    int right = 1; // 현재
+    int left = 1;   // 과거
+    int right = 1;  // 현재
 
-    while (left <= right and right <= MAX)
-    {
-        int G = right * right - left * left; // G 는 현재^2 - 과거^2
+    while (left <= right and right <= MAX) {
+        int G = right * right - left * left;  // G 는 현재^2 - 과거^2
 
         if (G < W)
             right++;
-        else if (G == W)
-        {
+        else if (G == W) {
             check = true;
             cout << right << '\n';
             right++;
-        }
-        else
-        {
+        } else {
             left++;
         }
     }
 
-    if (!check)
-    {
+    if (!check) {
         cout << -1 << '\n';
     }
     return 0;
