d57a6dd9a1c324a9ae4e246bb06c24c88a627f90
sdy <vel1024@khu.ac.kr>
2020-10-04 01:28:36 +0900

    rename file

--- cpp/FloydWarshall/FloydWarshall_khusw.cpp
+++ cpp/FloydWarshall/FloydWarshall_khusw.cpp
@@ -1,27 +1,37 @@
-
 #include <stdio.h>
-#define number 4
+#define number 5
 #define INF 1000000
 
-int a[4][4] = {
-    {0, 5, INF, 8},
-    {7, 0, 9, INF},
-    {2, INF, 0, 4},
-    {INF, INF, 3, 0}};
+int W[number][number] = {
+    {0, 1, INF, 1, 5},
+    {9, 0, 3, 2, INF},
+    {INF, INF, 0, 4, INF},
+    {INF, INF, 2, 0, 3},
+    {3, INF, INF, INF, 0}};
+int P[number][number];
+int D[number][number];
 
-void floydWarshall() {
-    int d[number][number];
+void path(int x, int y) {
+    if (P[x][y] != 0) {
+        path(x, P[x][y]);
+        printf("v%d", P[x][y]);
+        path(P[x][y], y);
+    }
+}
 
+void floydWarshall(int x, int y) {
     for (int i = 0; i < number; i++) {
         for (int j = 0; j < number; j++) {
-            d[i][j] = a[i][j];
+            D[i][j] = W[i][j];
         }
     }
+
     for (int k = 0; k < number; k++) {
         for (int i = 0; i < number; i++) {
             for (int j = 0; j < number; j++) {
-                if (d[i][k] + d[k][j] < d[i][j]) {
-                    d[i][j] = d[i][k] + d[k][j];
+                if (D[i][k] + D[k][j] < D[i][j]) {
+                    P[i][j] = k + 1;
+                    D[i][j] = D[i][k] + D[k][j];
                 }
             }
         }
@@ -29,13 +39,22 @@ void floydWarshall() {
 
     for (int i = 0; i < number; i++) {
         for (int j = 0; j < number; j++) {
-            printf("%3d ", d[i][j]);
+            printf("%3d ", D[i][j]);
         }
         printf("\n");
     }
+    printf("\n");
+    for (int i = 0; i < number; i++) {
+        for (int j = 0; j < number; j++) {
+            printf("%3d ", P[i][j]);
+        }
+        printf("\n");
+    }
+
+    path(x, y);
 }
 
 int main() {
-    floydWarshall();
+    floydWarshall(5, 3);
     return 0;
 }
