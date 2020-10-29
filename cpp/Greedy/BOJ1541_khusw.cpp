13c24d90dc4043c51a2f346243cf068fde052924
sdy <vel1024@khu.ac.kr>
2020-10-30 01:12:05 +0900

    create BOJ 1541

---
+++ cpp/Greedy/BOJ1541_khusw.cpp
@@ -0,0 +1,37 @@
+/*
+    BOJ 1541 잃어버린 괄호
+    https://www.acmicpc.net/problem/1541
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string input, tmp;
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    cin >> input;
+
+    int len = input.length(), answer = 0;
+    bool check = false;
+    for (int i = 0; i < len; ++i) {
+        if (input[i] == '+' || input[i] == '-' || input[i] == '\0') {
+            if (check)
+                answer -= stoi(tmp);
+            else
+                answer += stoi(tmp);
+
+            if (input[i] == '-') check = true;
+            tmp = "";
+            continue;
+        }
+        tmp += input[i];
+    }
+
+    cout << answer;
+
+    return 0;
+}
