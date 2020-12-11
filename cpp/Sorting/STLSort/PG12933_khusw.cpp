1814bec2f02c6ef17e2a5dd6f90cd10afde52cf3
sdy <vel1024@khu.ac.kr>
2020-12-11 23:25:27 +0900

    ㅊ

---
+++ cpp/Sorting/STLSort/PG12933_khusw.cpp
@@ -0,0 +1,21 @@
+/*
+    PG 12933 내림차순 정렬
+    https://programmers.co.kr/learn/courses/30/lessons/12933
+*/
+
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+bool comp(char ch1, char ch2) {
+    return ch1 > ch2;
+}
+
+long long solution(long long n) {
+    long long answer = 0;
+    string str = to_string(n);
+    sort(str.begin(), str.end(), comp);
+    answer = stoll(str);
+    return answer;
+}
