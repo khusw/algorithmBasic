a8f8b26d2fc616c11c0dd1000b3aa435594d0ab0
sdy <vel1024@khu.ac.kr>
2020-12-29 19:11:14 +0900

    create BOJ 11722

---
+++ cpp/DP/BOJ11722_khusw.cpp
@@ -0,0 +1,38 @@
+/*
+    BOJ 11722 가장 긴 감소하는 부분 수열
+    https://www.acmicpc.net/problem/11722
+*/
+
+#include <iostream>
+
+#define MAX 1001
+
+using namespace std;
+
+int N, answer;
+int arr[MAX], dp[MAX];
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    cin >> N;
+    for (int i = 0; i < N; ++i) cin >> arr[i];
+
+    for (int i = 0; i < N; ++i) {
+        dp[i] = 1;
+        for (int j = 0; j <= i; ++j) {
+            if (arr[i] < arr[j] && dp[j] + 1 > dp[i]) {
+                dp[i] = dp[j] + 1;
+            }
+        }
+    }
+
+    for (int i = 0; i < N; ++i)
+        if (answer < dp[i])
+            answer = dp[i];
+
+    cout << answer;
+
+    return 0;
+}
